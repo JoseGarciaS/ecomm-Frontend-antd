af5e55972b19291b8a84297ed160146bf00ef15f
JoseGarciaS <jargarcias@unitec.edu>
2022-12-15 17:21:07 -0600

    A

--- src/components/cartCard/cartCard.jsx
+++ src/components/cartCard/cartCard.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { Card, Skeleton, Image, List, Typography, Button } from 'antd';
+import React from "react";
+import { Card, Skeleton, Image, List, Typography, Button } from "antd";
+import { useState, useEffect } from "react";
+import { deleteItemFromOrder } from "../../functions/httpRequests/httpRequests";
+import { useSelector } from "react-redux";
 
-function OnClick(e) {}
+const App = (props) => {
+  function OnClick(e) {
+    setHidden(true);
+    deleteItemFromOrder(user.email, props.name);
+  }
 
-const App = (props) => (
+  let [hidden, setHidden] = useState(false);
+  const user = useSelector((state) => state.user).value;
+
+  return (
     <List.Item>
-        <Card title={props.name} style={{ width: '100%', height: 200 }}>
-            <Image src={props.imagePath} width={100} height={100} />
+      <Card
+        title={props.name}
+        style={{ width: "100%", height: 200 }}
+        loading={hidden}
+      >
+        <Image src={props.imagePath} width={100} height={100} />
 
-            <Typography.Text style={{ marginLeft: 30 }}>
-                Price: {props.price}
-            </Typography.Text>
+        <Typography.Text style={{ marginLeft: 30 }}>
+          Price: {props.price}
+        </Typography.Text>
 
-            <Button onClick={OnClick} style={{ marginLeft: 20 }}>
-                {' '}
-                Remove
-            </Button>
-        </Card>
+        <Button onClick={OnClick} style={{ marginLeft: 20 }}>
+          {" "}
+          Remove
+        </Button>
+      </Card>
     </List.Item>
-);
+  );
+};
 export default App;
